7479a28d4d5d9510b5a187681d04a6da93e57c80
Raiet Intraub <raietsaeed@gmail.com>
2018-06-04 21:57:52 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # puts "params = #{params}"
     if params[:name] && params[:name] != ""
-      # puts "success login"
       session[:name] = params[:name]
-      # puts "name = #{params[:name]} || #{session[:name]}"
       redirect_to '/'
     else
-      # puts "login failure"
       redirect_to '/login'
     end
   end
